a80f0e768e5cdca1f08f49dc305d60472847cf1a
Pascal <pascalwp68f@gmail.com>
2023-03-08 16:13:10 +0100

    --

---
+++ Defibrillateurs/main.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pwolff <pwolff@student.42mulhouse.fr>>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/08 16:12:48 by pwolff            #+#    #+#             */
+/*   Updated: 2023/03/08 16:12:48 by pwolff           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+int main()
+{
+    string lon;
+    cin >> lon; cin.ignore();
+    string lat;
+    cin >> lat; cin.ignore();
+    int n;
+    cin >> n; cin.ignore();
+    for (int i = 0; i < n; i++) {
+        string defib;
+        getline(cin, defib);
+    }
+
+    // Write an answer using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+
+    cout << "answer" << endl;
+}

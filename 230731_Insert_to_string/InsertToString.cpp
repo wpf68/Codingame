e9288d23f77ba2b21ba8dc93ad4cf77d8412913f
Pascal Wolff <pwolff@k0r1p9.42mulhouse.fr>
2023-08-02 10:55:49 +0200

    Insert To String  2 first OK

--- 230731_Insert_to_string/InsertToString.cpp
+++ 230731_Insert_to_string/InsertToString.cpp
@@ -67,6 +67,37 @@ std::string ft_push_s(std::string s, std::string tab, int number){
     return  newS;
 }
 
+std::vector<std::string>    ft_trie_tab(std::vector<std::string> tab, int change_count){
+
+    std::string temp;
+    int         test = 1;
+
+    while(test){
+        test = 0;
+        for (int i = 0; i < change_count - 1; i++){
+            if (ft_number(tab.at(i)) < ft_number(tab.at(i + 1))) {
+                temp = tab.at(i);
+                tab.at(i) = tab.at(i + 1);
+                tab.at(i + 1) = temp;
+                test = 1;
+            }
+        }
+    }
+
+
+        for (int i = 0; i < change_count - 1; i++){
+            if (ft_number(tab.at(i)) == ft_number(tab.at(i + 1))) {
+                temp = tab.at(i);
+                tab.at(i) = tab.at(i + 1);
+                tab.at(i + 1) = temp;
+            }
+        }
+
+
+
+    return tab;
+}    
+
 int main()
 {
     std::vector<std::string>    tab;
@@ -86,17 +117,48 @@ int main()
         std::cerr << i << " - " << tab.at(i) << std::endl;
     }
 
+    // ************   Trie du Tab  ***********
+    tab = ft_trie_tab(tab, change_count);
+    for (int i = 0; i < change_count; i++) {
+        std::cerr << i << " - " << tab.at(i) << std::endl;
+    }
+
+
+
     for (int i = 0; i < change_count; i++){
         int number = ft_number(tab.at(i));
 
         s = ft_push_s(s, tab.at(i), number);
-
     }
+
+
+
+    std::cerr << "\n*****  Resut *****\n" << std::endl;
+    //  ************  Taitement \n  ************
+    for (int i = 0; i < s.size(); i++){
+        if (s.at(i) == '\\'){
+            std::cerr << std::endl;
+            i++;
+        }
+        else
+            std::cerr << s.at(i);
+    }
+
+
+    //  ***********  tests  ***********
+
+
+    std::cerr << "\n*******  test  ********\n\n";
+    std::cout << "void main()" << std::endl;
+    std::cout << "{" << std::endl;
+    std::cout << "  Console.WriteLine(\"Hello World\");" << std::endl;
+    std::cout << "}" << std::endl;
 
     // Write an answer using cout. DON'T FORGET THE "<< endl"
     // To debug: cerr << "Debug messages..." << endl;
 
-    cout << "void main()" << endl;
-    cout << "answer2" << endl;
-    cout << "answer3" << endl;
+    // cout << "He said that I'm not good enough for the job. To which I replied \"Your lose!\"." << endl;
+    // cout << "answer2" << endl;
+    // cout << "answer3" << endl;
+    //std::cout << s << std::endl;
 }
